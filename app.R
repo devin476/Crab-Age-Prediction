8ca9557029b4b9e4a8a1453a07f26758d7c229e9
Devin <devinqs23@gmail.com>
2025-04-07 20:12:02 -0500

    WIP

--- app.R
+++ app.R
@@ -1,12 +1,9 @@
 library(tidyverse)
-library(ggplot2)
 library(class)
 library(caret)
-library(e1071)
+library(FNN)
 
 #Load
-
-CrabTest <- read.csv("Data/competition-1.csv")
 CrabTrain <- read.csv("Data/train-1.csv")
 
 #EDA
@@ -31,3 +28,64 @@ for (feature in features) {
 
 #Print
 print(cor_results)
+
+#Plotting
+#Shell Weight
+ggplot(CrabTrain, aes(x = Age, y = Shell.Weight)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = TRUE, color = "blue")
+
+#Height
+ggplot(CrabTrain, aes(x = Age, y = Height)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = TRUE, color = "blue")
+
+#Diameter
+ggplot(CrabTrain, aes(x = Age, y = Diameter)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = TRUE, color = "blue")
+
+#Length
+ggplot(CrabTrain, aes(x = Age, y = Length)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = TRUE, color = "blue")
+
+#Model Creation
+set.seed(7)
+#KNN
+feature_df <- CrabTrain %>%
+  select(Shell.Weight, Height, Diameter, Length, Weight, Age)
+
+#Standardize
+feature_df_scaled <- feature_df %>% mutate(across(where(is.numeric), scale))
+
+#Train-Test
+trainIndex <- createDataPartition(feature_df_scaled$Age, p = 0.7, list = FALSE)
+train_data <- feature_df_scaled[trainIndex, ]
+test_data <- feature_df_scaled[-trainIndex, ]
+
+train_x <- train_data %>% select(-Age)
+train_y <- train_data$Age
+test_x <- test_data %>% select(-Age)
+test_y <- test_data$Age
+
+#KNN Regression
+final_knn <- knn.reg(train = train_x, test = test_x, y = train_y, k = 34)
+
+#Predicted ages
+predicted_ages <- final_knn$pred
+
+#Actual ages
+actual_ages <- test_y
+
+#Evaluation
+rmse <- sqrt(mean((predicted_ages - actual_ages)^2))
+mae <- mean(abs(predicted_ages - actual_ages))
+sst <- sum((actual_ages - mean(actual_ages))^2)
+sse <- sum((predicted_ages - actual_ages)^2)
+r_squared <- 1 - (sse / sst)
+
+#Print
+cat("RMSE:",rmse, "\n")
+cat("MAE:",mae, "\n")
+cat("R-squared:",r_squared, "\n")
